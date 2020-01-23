ee2b47923c5cabbd06080629e810beda3c43f3e8
jared <jcraig1730@gmail.com>
2020-01-22 21:51:09 -0800

    Setup sale api routes

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,6 @@ router.use("/products", require("./products"));
 router.use("/vendors", require("./vendors"));
 router.use("/purchases", require("./purchases"));
 router.use("/customers", require("./customers"));
-// router.use("/sales", require("./sales"));
+router.use("/sales", require("./sales"));
 
 module.exports = router;
