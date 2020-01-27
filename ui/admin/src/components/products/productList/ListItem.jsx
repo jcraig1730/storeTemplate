8c818997fa2a54bce81a73ada125b3bcf6882e1e
jared <jcraig1730@gmail.com>
2020-01-26 17:43:13 -0800

    Start refactor to run admin and customer sites on different servers

---
+++ ui/admin/src/components/products/productList/ListItem.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import ConfirmDelete from "./ConfirmDelete";
+import { deleteItem } from "../../../apiCalls";
+
+const ListItem = ({ product, updateProducts, setIsError }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseOver = e =>
+    e === "enter" ? setIsHovered(true) : setIsHovered(false);
+
+  const deleteProduct = async e => {
+    try {
+      const result = await deleteItem({
+        category: "products",
+        data: { id: product._id }
+      });
+      if (result.status !== 204) {
+        throw {
+          title: "Could Not Delete",
+          message:
+            "An error has occured in deleting this item. Please try again."
+        };
+      }
+      updateProducts();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div
+      className="list-group-item"
+      onMouseEnter={() => handleMouseOver("enter")}
+      onMouseLeave={handleMouseOver}
+    >
+      <div className={`${isHovered ? "visible" : "invisible"} float-right`}>
+        <div data-toggle="modal" data-target={`#confirm-delete-${product._id}`}>
+          <i className="fas fa-trash text-danger"></i>
+        </div>
+      </div>
+      <div className={`${isHovered ? "visible" : "invisible"} float-right`}>
+        <i className="fas fa-edit pr-3 text-primary"></i>
+      </div>
+      <div>Title: {product.title}</div>
+      <div>Description: {product.description}</div>
+      <div>Qty: {product.quantity}</div>
+      {<ConfirmDelete product={product} deleteProduct={deleteProduct} />}
+    </div>
+  );
+};
+
+ConfirmDelete.propTypes = {
+  product: PropTypes.object.isRequired,
+  updateProducts: PropTypes.func.isRequired
+};
+
+export default ListItem;
---
+++ ui/admin/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "regenerator-runtime";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
