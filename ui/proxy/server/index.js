3d4a30d51ec5c1b97409e3ec12f86a9ef156331d
jared <jcraig1730@gmail.com>
2020-01-30 14:29:47 -0800

    Comment out main store route until it is set up

---
+++ ui/proxy/server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/admin*", (req, res) => {
+  app.use(express.static(path.join(__dirname, "../dist")));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+// app.use("/", (req, res) => {
+//   app.use(express.static(path.join(__dirname, "../customerDist")));
+//   res.sendFile(path.join(__dirname, "../customerDist/index.html"));
+// });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Store running on port ${port}`));
